0135492a8071f7e46504bedd71ecbbf3cc11f192
Adriano Spalenza <108499428+adrianospalenzaa@users.noreply.github.com>
2023-09-09 15:53:09 -0300

    Atualização

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World "); // print não quebra linha
+        System.out.println("Bom dia");  // println quebra linha
+    }
+}
